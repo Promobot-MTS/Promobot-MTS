62789b84e67cb315e1df70966c5ad803f3d96ccf
Quod <36698256+Quod6@users.noreply.github.com>
2020-03-09 19:38:43 +1000

    Add files via upload

--- Lidar.py
+++ Lidar.py
@@ -1,5 +1,6 @@
 import rplidar as rp
 from PIL import Image as img
+import cv2
 import numpy as np
 import time
 import threading as tr
@@ -9,13 +10,27 @@ class PromobotLidar():
     LeftSectBord = [285, 335]
     ForwardSectBord = [335, 25]
     RightSectBord = [25, 75]
+    img = np.zeros((600, 800, 3))
 
+    PORT1 = "COM3"
     flagdraw = False
 
     def __init__(self, PORT):
-        self.lidar = rp.RPLidar(PORT, 115200)
+        # self.lidar = rp.RPLidar(PORT, 115200)
+        while 1:
+            try:
+                self.lidar = rp.RPLidar(PORT, 115200)
+                self.PORT1 = PORT
+                break
+            except:
+                pass
 
-    def searchforward(self, interval):
+    def restart(self):
+        self.lidar.disconnect()
+        self.__init__(self.PORT1)
+        pass
+
+    def lookforward(self, interval):
         AngDist = []
         distances = []
         self.lidar.clear_input()
@@ -31,42 +46,48 @@ class PromobotLidar():
             except Exception as E:
                 print(E)
 
+
     def looksectors(self):
         LeftDistSects = []
         ForwardDistSects = []
         RightDistSects = []
-
-        for scan in self.lidar.iter_scans():
-            try:
-                for i in range(len(scan)):
-                    if scan[i][1] >= self.LeftSectBord[0] and scan[i][1] <= self.LeftSectBord[1]:
-                        LeftDistSects.append(scan[i][2])
-                    elif scan[i][1] > self.ForwardSectBord[0] or scan[i][1] < self.ForwardSectBord[1]:
-                        ForwardDistSects.append(scan[i][2])
-                    elif scan[i][1] >= self.RightSectBord[0] or scan[i][1] <= self.RightSectBord[1]:
-                        RightDistSects.append(scan[i][2])
-                    else:
-                        continue
-
-                return min(LeftDistSects), min(ForwardDistSects), min(RightDistSects)
-            except Exception as E:
-                print(E)
+        try:
+            for scan in self.lidar.iter_scans():
+                try:
+                    for i in range(len(scan)):
+                        if scan[i][1] >= self.LeftSectBord[0] and scan[i][1] <= self.LeftSectBord[1]:
+                            LeftDistSects.append(scan[i][2])
+                        elif scan[i][1] > self.ForwardSectBord[0] or scan[i][1] < self.ForwardSectBord[1]:
+                            ForwardDistSects.append(scan[i][2])
+                        elif scan[i][1] >= self.RightSectBord[0] or scan[i][1] <= self.RightSectBord[1]:
+                            RightDistSects.append(scan[i][2])
+                        else:
+                            continue
+                    return min(LeftDistSects), min(ForwardDistSects), min(RightDistSects)
+                except Exception as E:
+                    print(E)
+        except:
+            self.lidar.stop()
+            self.restart()
 
 
     def startdrawmap(self, width, height):
         if self.flagdraw == False:
             self.flagdraw = True
-        map = img.new('RGB', (width, height), (0, 0, 120))
-        centerX = int(width/2)
-        centerY = int(height/2)
-        while self.flagdraw:
-            map.show()
+        pass
+
 
     def stopdrawmap(self):
         self.flagdraw = False
 
 
 if __name__ == '__main__':
-    lidar1 = PromobotLidar("COM3")
-    r = lidar1.looksectors()
+    li = PromobotLidar("COM3")
+    while 1:
+        r = li.looksectors()
+        if r == None:
+            li.restart()
+        else:
+            break
+
     print(r)
